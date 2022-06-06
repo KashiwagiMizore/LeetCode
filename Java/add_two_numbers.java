2bb8d8c572057e1c06b43f2a4c776dd7c4d98e41
Pleiades <57717406+KashiwagiMizore@users.noreply.github.com>
2022-06-06 22:02:48 +0900

    Update add_two_numbers.java

--- Java/add_two_numbers.java
+++ Java/add_two_numbers.java
@@ -22,24 +22,26 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = null, tail = null;
         int x, y, total;
-        int carry = 0;
+        int carry = 0; //定义进位
         while (l1 != null || l2 != null) {
             if (l1 != null) x = l1.val;
             else x = 0;
             if (l2 != null) y = l2.val;
             else y = 0;
             total = x + y + carry;
-            carry = total / 10;
+            carry = total / 10;//计算进位
+            //注意头节点为空的特殊处理
             if (head == null) {
                 head = tail = new ListNode(total % 10);
             } else {
                 tail.next = new ListNode(total % 10);
                 tail = tail.next;
             }
+            //不为空时继续
             if (l1 != null) l1 = l1.next;
             if (l2 != null) l2 = l2.next;
         }
-        if (carry > 0) tail.next = new ListNode(carry);
+        if (carry > 0) tail.next = new ListNode(carry);//给最后的节点赋值最后计算完有无进位
         return head;
     }
 }
